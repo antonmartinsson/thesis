ade81b493b4506fe97263d5a9f20e215d8730f4f
antonmedstorta <aejmartinsson@gmail.com>
2019-04-27 11:10:46 +0200

    All Likert data so far added

--- stackedLikert.js
+++ stackedLikert.js
@@ -1,3 +1,5 @@
+createLikert();
+
 var margin = {top: 50, right: 20, bottom: 10, left: 65},
     width = 800 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -24,111 +26,335 @@ var svg = d3.select("#likertContainer").append("svg")
     .attr("height", height + margin.top + margin.bottom)
     .attr("id", "d3-plot")
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+    .attr("id", "graphicContainer");
 
   likertColors.domain(["Strongly disagree", "Disagree", "Neither agree nor disagree", "Agree", "Strongly agree"]);
 
-  d3.csv("raw_data.csv", function(error, data) {
-    console.log(data);
+  function cleanAndCreate() {
+    var svg = d3.select("#graphicContainer");
+    svg.selectAll("*").remove();
+    createLikert();
+  }
 
-  data.forEach(function(d) {
-    // calc percentages
-    d["Strongly disagree"] = +d[1]*100/d.N;
-    d["Disagree"] = +d[2]*100/d.N;
-    d["Neither agree nor disagree"] = +d[3]*100/d.N;
-    d["Agree"] = +d[4]*100/d.N;
-    d["Strongly agree"] = +d[5]*100/d.N;
-    var x0 = -1*(d["Neither agree nor disagree"]/2+d["Disagree"]+d["Strongly disagree"]);
-    var idx = 0;
-    d.boxes = likertColors.domain().map(function(name) { return {name: name, x0: x0, x1: x0 += +d[name], N: +d.N, n: +d[idx += 1]}; });
-  });
+  function createLikert() {
+    
+    var implementation = document.getElementById("implementation").value;
 
-  var min_val = d3.min(data, function(d) {
-          return d.boxes["0"].x0;
-          });
+    if (implementation == "panel") {
+      d3.csv("panel_likert.csv", function(error, data) {
+        console.log(data);
 
-  var max_val = d3.max(data, function(d) {
-          return d.boxes["4"].x1;
-          });
+        data.forEach(function(d) {
+          // calc percentages
+          d["Strongly disagree"] = +d[1]*100/d.N;
+          d["Disagree"] = +d[2]*100/d.N;
+          d["Neither agree nor disagree"] = +d[3]*100/d.N;
+          d["Agree"] = +d[4]*100/d.N;
+          d["Strongly agree"] = +d[5]*100/d.N;
+          var x0 = -1*(d["Neither agree nor disagree"]/2+d["Disagree"]+d["Strongly disagree"]);
+          var idx = 0;
+          d.boxes = likertColors.domain().map(function(name) { return {name: name, x0: x0, x1: x0 += +d[name], N: +d.N, n: +d[idx += 1]}; });
+        });
+      
+        var min_val = d3.min(data, function(d) {
+                return d.boxes["0"].x0;
+                });
+      
+        var max_val = d3.max(data, function(d) {
+                return d.boxes["4"].x1;
+                });
+      
+        x.domain([min_val, max_val]).nice();
+        y.domain(data.map(function(d) { return d.Question; }));
+      
+        var vakken = svg.selectAll(".question")
+            .data(data)
+          .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(0," + y(d.Question) + ")"; });
+      
+        var bars = vakken.selectAll("rect")
+            .data(function(d) { return d.boxes; })
+          .enter().append("g").attr("class", "subbar");
+      
+        bars.append("rect")
+            .attr("height", y.rangeBand())
+            .attr("x", function(d) { return x(d.x0); })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0); })
+            .style("fill", function(d) { return likertColors(d.name); });
+      
+        bars.append("text")
+            .attr("x", function(d) { return x(d.x0); })
+            .attr("y", y.rangeBand()/2)
+            .attr("dy", "0.5em")
+            .attr("dx", "0.5em")
+            .style("font" ,"10px sans-serif")
+            .style("text-anchor", "begin")
+            .text(function(d) { return d.n !== 0 && (d.x1-d.x0)>3 ? d.n : "" });
+      
+        vakken.insert("rect",":first-child")
+            .attr("height", y.rangeBand())
+            .attr("x", "1")
+            .attr("width", width)
+            .attr("fill-opacity", "0.5")
+            .style("fill", "#F5F5F5")
+            .attr("class", function(d,index) { return index%2==0 ? "even" : "uneven"; });
+      
+        svg.append("g")
+            .attr("class", "y axis")
+        .append("line")
+            .attr("x1", x(0))
+            .attr("x2", x(0))
+            .attr("y2", height);
+      
+        var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
+        // this is not nice, we should calculate the bounding box and use that
+        var legend_tabs = [0, 120, 200, 375, 450];
+        var legend = startp.selectAll(".legend")
+            .data(likertColors.domain().slice())
+          .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(" + legend_tabs[i] + ",-45)"; });
+      
+        legend.append("rect")
+            .attr("x", 0)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", likertColors);
+      
+        legend.append("text")
+            .attr("x", 22)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "begin")
+            .style("font" ,"10px sans-serif")
+            .text(function(d) { return d; });
+      
+        d3.selectAll(".axis path")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges")
+      
+        d3.selectAll(".axis line")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges")
+      
+        var movesize = width/2 - startp.node().getBBox().width/2;
+        d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
+     
+      });
+    }
+    else if (implementation == "popup") {
+      d3.csv("popup_likert.csv", function(error, data) {
+        console.log(data);
 
-  x.domain([min_val, max_val]).nice();
-  y.domain(data.map(function(d) { return d.Question; }));
+        data.forEach(function(d) {
+          // calc percentages
+          d["Strongly disagree"] = +d[1]*100/d.N;
+          d["Disagree"] = +d[2]*100/d.N;
+          d["Neither agree nor disagree"] = +d[3]*100/d.N;
+          d["Agree"] = +d[4]*100/d.N;
+          d["Strongly agree"] = +d[5]*100/d.N;
+          var x0 = -1*(d["Neither agree nor disagree"]/2+d["Disagree"]+d["Strongly disagree"]);
+          var idx = 0;
+          d.boxes = likertColors.domain().map(function(name) { return {name: name, x0: x0, x1: x0 += +d[name], N: +d.N, n: +d[idx += 1]}; });
+        });
+      
+        var min_val = d3.min(data, function(d) {
+                return d.boxes["0"].x0;
+                });
+      
+        var max_val = d3.max(data, function(d) {
+                return d.boxes["4"].x1;
+                });
+      
+        x.domain([min_val, max_val]).nice();
+        y.domain(data.map(function(d) { return d.Question; }));
+      
+        var vakken = svg.selectAll(".question")
+            .data(data)
+          .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(0," + y(d.Question) + ")"; });
+      
+        var bars = vakken.selectAll("rect")
+            .data(function(d) { return d.boxes; })
+          .enter().append("g").attr("class", "subbar");
+      
+        bars.append("rect")
+            .attr("height", y.rangeBand())
+            .attr("x", function(d) { return x(d.x0); })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0); })
+            .style("fill", function(d) { return likertColors(d.name); });
+      
+        bars.append("text")
+            .attr("x", function(d) { return x(d.x0); })
+            .attr("y", y.rangeBand()/2)
+            .attr("dy", "0.5em")
+            .attr("dx", "0.5em")
+            .style("font" ,"10px sans-serif")
+            .style("text-anchor", "begin")
+            .text(function(d) { return d.n !== 0 && (d.x1-d.x0)>3 ? d.n : "" });
+      
+        vakken.insert("rect",":first-child")
+            .attr("height", y.rangeBand())
+            .attr("x", "1")
+            .attr("width", width)
+            .attr("fill-opacity", "0.5")
+            .style("fill", "#F5F5F5")
+            .attr("class", function(d,index) { return index%2==0 ? "even" : "uneven"; });
+      
+        svg.append("g")
+            .attr("class", "y axis")
+        .append("line")
+            .attr("x1", x(0))
+            .attr("x2", x(0))
+            .attr("y2", height);
+      
+        var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
+        // this is not nice, we should calculate the bounding box and use that
+        var legend_tabs = [0, 120, 200, 375, 450];
+        var legend = startp.selectAll(".legend")
+            .data(likertColors.domain().slice())
+          .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(" + legend_tabs[i] + ",-45)"; });
+      
+        legend.append("rect")
+            .attr("x", 0)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", likertColors);
+      
+        legend.append("text")
+            .attr("x", 22)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "begin")
+            .style("font" ,"10px sans-serif")
+            .text(function(d) { return d; });
+      
+        d3.selectAll(".axis path")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges")
+      
+        d3.selectAll(".axis line")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges")
+      
+        var movesize = width/2 - startp.node().getBBox().width/2;
+        d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
+     
+      });
+    }
+    else if (implementation == "enlarge") {
+      d3.csv("enlarge_likert.csv", function(error, data) {
+        console.log(data);
 
-  var vakken = svg.selectAll(".question")
-      .data(data)
-    .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) { return "translate(0," + y(d.Question) + ")"; });
-
-  var bars = vakken.selectAll("rect")
-      .data(function(d) { return d.boxes; })
-    .enter().append("g").attr("class", "subbar");
-
-  bars.append("rect")
-      .attr("height", y.rangeBand())
-      .attr("x", function(d) { return x(d.x0); })
-      .attr("width", function(d) { return x(d.x1) - x(d.x0); })
-      .style("fill", function(d) { return likertColors(d.name); });
-
-  bars.append("text")
-      .attr("x", function(d) { return x(d.x0); })
-      .attr("y", y.rangeBand()/2)
-      .attr("dy", "0.5em")
-      .attr("dx", "0.5em")
-      .style("font" ,"10px sans-serif")
-      .style("text-anchor", "begin")
-      .text(function(d) { return d.n !== 0 && (d.x1-d.x0)>3 ? d.n : "" });
-
-  vakken.insert("rect",":first-child")
-      .attr("height", y.rangeBand())
-      .attr("x", "1")
-      .attr("width", width)
-      .attr("fill-opacity", "0.5")
-      .style("fill", "#F5F5F5")
-      .attr("class", function(d,index) { return index%2==0 ? "even" : "uneven"; });
-
-  svg.append("g")
-      .attr("class", "y axis")
-  .append("line")
-      .attr("x1", x(0))
-      .attr("x2", x(0))
-      .attr("y2", height);
-
-  var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
-  // this is not nice, we should calculate the bounding box and use that
-  var legend_tabs = [0, 120, 200, 375, 450];
-  var legend = startp.selectAll(".legend")
-      .data(likertColors.domain().slice())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(" + legend_tabs[i] + ",-45)"; });
-
-  legend.append("rect")
-      .attr("x", 0)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", likertColors);
-
-  legend.append("text")
-      .attr("x", 22)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "begin")
-      .style("font" ,"10px sans-serif")
-      .text(function(d) { return d; });
-
-  d3.selectAll(".axis path")
-      .style("fill", "none")
-      .style("stroke", "#000")
-      .style("shape-rendering", "crispEdges")
-
-  d3.selectAll(".axis line")
-      .style("fill", "none")
-      .style("stroke", "#000")
-      .style("shape-rendering", "crispEdges")
-
-  var movesize = width/2 - startp.node().getBBox().width/2;
-  d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
-
-
-});
+        data.forEach(function(d) {
+          // calc percentages
+          d["Strongly disagree"] = +d[1]*100/d.N;
+          d["Disagree"] = +d[2]*100/d.N;
+          d["Neither agree nor disagree"] = +d[3]*100/d.N;
+          d["Agree"] = +d[4]*100/d.N;
+          d["Strongly agree"] = +d[5]*100/d.N;
+          var x0 = -1*(d["Neither agree nor disagree"]/2+d["Disagree"]+d["Strongly disagree"]);
+          var idx = 0;
+          d.boxes = likertColors.domain().map(function(name) { return {name: name, x0: x0, x1: x0 += +d[name], N: +d.N, n: +d[idx += 1]}; });
+        });
+      
+        var min_val = d3.min(data, function(d) {
+                return d.boxes["0"].x0;
+                });
+      
+        var max_val = d3.max(data, function(d) {
+                return d.boxes["4"].x1;
+                });
+      
+        x.domain([min_val, max_val]).nice();
+        y.domain(data.map(function(d) { return d.Question; }));
+      
+        var vakken = svg.selectAll(".question")
+            .data(data)
+          .enter().append("g")
+            .attr("class", "bar")
+            .attr("transform", function(d) { return "translate(0," + y(d.Question) + ")"; });
+      
+        var bars = vakken.selectAll("rect")
+            .data(function(d) { return d.boxes; })
+          .enter().append("g").attr("class", "subbar");
+      
+        bars.append("rect")
+            .attr("height", y.rangeBand())
+            .attr("x", function(d) { return x(d.x0); })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0); })
+            .style("fill", function(d) { return likertColors(d.name); });
+      
+        bars.append("text")
+            .attr("x", function(d) { return x(d.x0); })
+            .attr("y", y.rangeBand()/2)
+            .attr("dy", "0.5em")
+            .attr("dx", "0.5em")
+            .style("font" ,"10px sans-serif")
+            .style("text-anchor", "begin")
+            .text(function(d) { return d.n !== 0 && (d.x1-d.x0)>3 ? d.n : "" });
+      
+        vakken.insert("rect",":first-child")
+            .attr("height", y.rangeBand())
+            .attr("x", "1")
+            .attr("width", width)
+            .attr("fill-opacity", "0.5")
+            .style("fill", "#F5F5F5")
+            .attr("class", function(d,index) { return index%2==0 ? "even" : "uneven"; });
+      
+        svg.append("g")
+            .attr("class", "y axis")
+        .append("line")
+            .attr("x1", x(0))
+            .attr("x2", x(0))
+            .attr("y2", height);
+      
+        var startp = svg.append("g").attr("class", "legendbox").attr("id", "mylegendbox");
+        // this is not nice, we should calculate the bounding box and use that
+        var legend_tabs = [0, 120, 200, 375, 450];
+        var legend = startp.selectAll(".legend")
+            .data(likertColors.domain().slice())
+          .enter().append("g")
+            .attr("class", "legend")
+            .attr("transform", function(d, i) { return "translate(" + legend_tabs[i] + ",-45)"; });
+      
+        legend.append("rect")
+            .attr("x", 0)
+            .attr("width", 18)
+            .attr("height", 18)
+            .style("fill", likertColors);
+      
+        legend.append("text")
+            .attr("x", 22)
+            .attr("y", 9)
+            .attr("dy", ".35em")
+            .style("text-anchor", "begin")
+            .style("font" ,"10px sans-serif")
+            .text(function(d) { return d; });
+      
+        d3.selectAll(".axis path")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges")
+      
+        d3.selectAll(".axis line")
+            .style("fill", "none")
+            .style("stroke", "#000")
+            .style("shape-rendering", "crispEdges")
+      
+        var movesize = width/2 - startp.node().getBBox().width/2;
+        d3.selectAll(".legendbox").attr("transform", "translate(" + movesize  + ",0)");
+     
+      });
+    }
+   
+}
